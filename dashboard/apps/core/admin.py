e76301f72c9868f133dac904e6515446ce92161c
Aman Bhargava <aman56493@gmail.com>
2020-10-29 19:53:04 +0530

    modify models

--- dashboard/apps/core/admin.py
+++ dashboard/apps/core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+from dashboard.apps.core.models import DataWindow
+
+
+class DataWindowResource(resources.ModelResource):
+	class Meta:
+		model = DataWindow
+
+
+class DataWindowAdmin(ImportExportModelAdmin):
+	resource_class = DataWindowResource
+
+
+admin.site.register(DataWindow, DataWindowAdmin)
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -29,146 +29,144 @@ SECRET_KEY = 'p_^jnpqsmo9u@y4vso!&3!ablvk1+=0t+n50th$_yn6bw(0u19'
 DEBUG = os.environ.get('DEBUG', 0)
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-        },
-        'logfile': {
-            'class': 'logging.handlers.WatchedFileHandler',
-            'filename': BASE_DIR + '/django.log'
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': './debug.log',
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'loggers': {
-        'main': {
-            'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-            'propagate': True,
-        },
-        'django': {
-            'handlers': ['logfile'],
-            'level': 'ERROR',
-            'propagate': False
-        }
-    }
+	'version': 1,
+	'disable_existing_loggers': False,
+	'handlers': {
+		'console': {
+			'class': 'logging.StreamHandler',
+		},
+		'logfile': {
+			'class': 'logging.handlers.WatchedFileHandler',
+			'filename': BASE_DIR + '/django.log'
+		},
+		'file': {
+			'level': 'DEBUG',
+			'class': 'logging.FileHandler',
+			'filename': './debug.log',
+		},
+	},
+	'filters': {
+		'require_debug_false': {
+			'()': 'django.utils.log.RequireDebugFalse'
+		}
+	},
+	'loggers': {
+		'main': {
+			'handlers': ['console'],
+			'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+			'propagate': True,
+		},
+		'django': {
+			'handlers': ['logfile'],
+			'level': 'ERROR',
+			'propagate': False
+		}
+	}
 }
 
 ALLOWED_HOSTS = [
-    'localhost'
+	'localhost'
 ]
-
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 
-    'dashboard.apps.core',
-    'dashboard.apps.frontend',
+	'dashboard.apps.core',
+	'dashboard.apps.frontend',
 
-    'dashboard.apps.components',
-    'dashboard.apps.components.buttons',
-    'dashboard.apps.components.cards',
+	'dashboard.apps.components',
+	'dashboard.apps.components.buttons',
+	'dashboard.apps.components.cards',
 
-    'dashboard.apps.utilities',
-    'dashboard.apps.utilities.colors',
-    'dashboard.apps.utilities.borders',
-    'dashboard.apps.utilities.animations',
-    'dashboard.apps.utilities.others',
+	'dashboard.apps.utilities',
+	'dashboard.apps.utilities.colors',
+	'dashboard.apps.utilities.borders',
+	'dashboard.apps.utilities.animations',
+	'dashboard.apps.utilities.others',
 
-    'dashboard.apps.pages.blank',
-    'dashboard.apps.pages.login',
-    'dashboard.apps.pages.pagenotfound',
-    'dashboard.apps.pages.password',
-    'dashboard.apps.pages.register',
+	'dashboard.apps.pages.blank',
+	'dashboard.apps.pages.login',
+	'dashboard.apps.pages.pagenotfound',
+	'dashboard.apps.pages.password',
+	'dashboard.apps.pages.register',
 
-    'dashboard.apps.pages.charts',
-    'dashboard.apps.pages.tables',
+	'dashboard.apps.pages.charts',
+	'dashboard.apps.pages.tables',
 
-    'django_extensions'
+	'django_extensions',
+	'import_export'
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'dashboard.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            APP_FLDR + '/templates',
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
+	{
+		'BACKEND': 'django.template.backends.django.DjangoTemplates',
+		'DIRS': [
+			APP_FLDR + '/templates',
+		],
+		'APP_DIRS': True,
+		'OPTIONS': {
+			'context_processors': [
+				'django.template.context_processors.debug',
+				'django.template.context_processors.request',
+				'django.contrib.auth.context_processors.auth',
+				'django.contrib.messages.context_processors.messages',
+			],
+		},
+	},
 ]
 
 WSGI_APPLICATION = 'dashboard.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'postgres',
+		'USER': 'postgres',
+		'PASSWORD': 'postgres',
+		'HOST': 'db',
+		'PORT': 5432,
+	}
 }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+	{
+		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+	},
+	{
+		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+	},
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -183,13 +181,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, APP_NAME + '/static')
+	os.path.join(BASE_DIR, APP_NAME + '/static')
 ]
 
 #
