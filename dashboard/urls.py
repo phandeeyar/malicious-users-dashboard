1bc3026efeb6ae13fee26f06f2de24f6587a92b5
Aman Bhargava <aman56493@gmail.com>
2020-11-01 00:25:21 +0530

    add login view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
 from django.urls import path
+from django.conf.urls import url
 from django.urls.conf import include
+import django.contrib.auth.views as auth_views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', 		include('dashboard.apps.urls')),
-    path('admin/',	admin.site.urls),
+	path('', include('dashboard.apps.urls')),
+	url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name='login/login.html')),
+	path('admin/', admin.site.urls),
 ]
