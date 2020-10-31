fd2ab38586327dd30e6f71d09c3b92635706b78f
Aman Bhargava <aman56493@gmail.com>
2020-10-31 23:35:11 +0530

    Clean view

--- dashboard/apps/frontend/views.py
+++ dashboard/apps/frontend/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-
+from django import forms
 from datetime import date, timedelta
 from dashboard.apps.core.models import DataWindow, MaliciousUser, WordCloud, TargetGroup
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core.serializers import serialize
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class IndexView(generic.ListView):
+class IndexView(LoginRequiredMixin, generic.base.TemplateView):
 	""""
     IndexView:
     """
@@ -15,45 +16,52 @@ class IndexView(generic.ListView):
 	module = 'indexView'
 	template_name = 'frontend/index.html'
 
-	def get_queryset(self):
-		start_date = self.request.GET.get("start_date", date.today() - timedelta(days=7))
-		end_date = self.request.GET.get("end_date", date.today())
-		return DataWindow.objects.filter(date__gte=start_date, date__lte=end_date)
+	@staticmethod
+	def get_data_window_objects(date_range_lower, date_range_upper):
+		return DataWindow.objects.filter(date__gte=date_range_lower, date__lte=date_range_upper)
 
-	def get_malicious_users(self):
-		start_date = self.request.GET.get("start_date", date.today() - timedelta(days=7))
-		end_date = self.request.GET.get("end_date", date.today())
-		return MaliciousUser.objects.filter(date__gte=start_date, date__lte=end_date)
-	
-	def get_target_group_data(self):
-		start_date = self.request.GET.get("start_date", date.today() - timedelta(days=7))
-		end_date = self.request.GET.get("end_date", date.today())
-		return TargetGroup.objects.filter(date__gte=start_date, date__lte=end_date)
+	@staticmethod
+	def get_malicious_users(date_range_lower, date_range_upper):
+		return MaliciousUser.objects.filter(date__gte=date_range_lower, date__lte=date_range_upper)
 
-	def get_word_cloud_data(self):
-		start_date = self.request.GET.get("start_date", date.today() - timedelta(days=7))
-		end_date = self.request.GET.get("end_date", date.today())
-		return WordCloud.objects.filter(date__gte=start_date, date__lte=end_date)
+	@staticmethod
+	def get_target_group_data(date_range_lower, date_range_upper):
+		return TargetGroup.objects.filter(date__gte=date_range_lower, date__lte=date_range_upper)
+
+	@staticmethod
+	def get_word_cloud_data(date_range_lower, date_range_upper):
+		return WordCloud.objects.filter(date__gte=date_range_lower, date__lte=date_range_upper)
 
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		queryset = self.get_queryset()
-		malicious_users_queryset = self.get_malicious_users()
+		dtl = self.request.GET.get("start_date", date.today() - timedelta(days=7))  # Upper Date Range
+		if dtl == "":
+			date_range_lower = date.today() - timedelta(days=7)
+		else:
+			date_range_lower = dtl
+		dtu = self.request.GET.get("end_date", date.today())  # Lower Date Range
+		if dtu == "":
+			date_range_upper = date.today()
+		else:
+			date_range_upper = dtu
+		data_window_queryset = self.get_data_window_objects(date_range_lower, date_range_upper)
+		malicious_users_queryset = self.get_malicious_users(date_range_lower, date_range_upper)
+		context['targeted_groups_data'] = self.get_target_group_data(date_range_lower, date_range_upper)
 		# Word Cloud Data
-		context['word_cloud_data'] = serialize('json', self.get_word_cloud_data(), cls=DjangoJSONEncoder)
+		context['word_cloud_data'] = serialize('json', self.get_word_cloud_data(date_range_lower, date_range_upper), cls=DjangoJSONEncoder)
 		top_20_malicious_users = malicious_users_queryset.order_by('-malicious_score')[:20]
 		# Serialize data and send in context
 		top_20_malicious_users = serialize('json', top_20_malicious_users, cls=DjangoJSONEncoder)
 		context['top_20_malicious_users'] = top_20_malicious_users
 		# number of posts in the selected date range
-		context['number_of_posts'] = len(queryset.distinct('post_url'))
+		context['number_of_posts'] = len(data_window_queryset.distinct('post_url'))
 		# Number of comments in the selected date range
-		context['number_of_comments'] = len(queryset)
+		context['number_of_comments'] = len(data_window_queryset)
 		try:
-			context['hate_speech_percent'] = (len(queryset.filter(hate_speech=True)) / len(queryset) * 100)
+			context['hate_speech_percent'] = (len(data_window_queryset.filter(hate_speech=True)) / len(data_window_queryset) * 100)
 		except ZeroDivisionError:
 			context['hate_speech_percent'] = 0
-		context['targeted_groups_data'] = self.get_target_group_data()
-		context['num_unique_users'] = len(queryset.distinct('profile_id'))
+		
+		context['num_unique_users'] = len(data_window_queryset.distinct('profile_id'))
 
 		return context
