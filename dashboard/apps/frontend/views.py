75d058eaaaba2b60020eb4c7c08b9e2b85548a89
Aman Bhargava <aman56493@gmail.com>
2020-10-31 20:44:22 +0530

    modify copyright

--- dashboard/apps/frontend/views.py
+++ dashboard/apps/frontend/views.py
@@ -15,42 +15,6 @@ class IndexView(generic.ListView):
 	module = 'indexView'
 	template_name = 'frontend/index.html'
 
-	data = [
-		['#', 'Header', 'Header', 'Header', 'Header'],
-		['1,001', 'Lorem', 'ipsum', 'dolor', 'sit'],
-		['1,002', 'amet', 'consectetur', 'adipiscing', 'elit'],
-		['1,003', 'Integer', 'nec', 'odio', 'Praesent'],
-		['1,003', 'libero', 'Sed', 'cursus', 'ante'],
-		['1,004', 'dapibus', 'diam', 'Sed', 'nisi'],
-		['1,005', 'Nulla', 'quis', 'sem', 'at'],
-		['1,006', 'nibh', 'elementum', 'imperdit', 'Duis'],
-		['1,007', 'sagittis', 'ipsum', 'Praesent', 'mauris'],
-		['1,008', 'Fusce', 'nec', 'tellus', 'sed'],
-		['1,009', 'augue', 'semper', 'porta', 'Mauris'],
-		['1,010', 'massa', 'Vestibulum', 'lacinia', 'arcu'],
-		['1,011', 'eget', 'nulla', 'Class', 'aptent'],
-		['1,012', 'taciti', 'sociosqu', 'ad', 'litora'],
-		['1,013', 'torquent', 'per', 'conubia', 'nostra'],
-		['1,014', 'per', 'inceptos', 'himenaeos', 'Curabtur'],
-		['1,015', 'sodales', 'ligula', 'in', 'libero'],
-	]
-
-	projects_original = [
-		['Server Migration', '20%', 'bg-danger', '20'],
-		['Sales Tracking', '40%', 'bg-warning', '40'],
-		['Customer Database', '60%', '', '60'],
-		['Payout Details', '80%', 'bg-info', '80'],
-		['Account Setup', 'Complete!', 'bg-success', '100']
-	]
-
-	projects = [
-		['Server Migration', '10%', 'bg-danger', '20'],
-		['Sales Tracking', '70%', 'bg-info', '40'],
-		['Customer Database', '60%', '', '60'],
-		['Payout Details', 'Complete!', 'bg-success', '100'],
-		['Account Setup', '40%', 'bg-warning', '40']
-	]
-
 	def get_queryset(self):
 		start_date = self.request.GET.get("start_date", date.today() - timedelta(days=7))
 		end_date = self.request.GET.get("end_date", date.today())
@@ -91,6 +55,5 @@ class IndexView(generic.ListView):
 			context['hate_speech_percent'] = 0
 		context['targeted_groups_data'] = self.get_target_group_data()
 		context['num_unique_users'] = len(queryset.distinct('profile_id'))
-		context['projects'] = self.projects
 
 		return context
